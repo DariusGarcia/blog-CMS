a448f27cac8a98b5b757f8dfa64631bef1ae3299
Darius Garcia <dariusgarcia888@gmail.com>
2023-01-02 21:51:07 -0800

    updated CSS for views

--- public/js/post.js
+++ public/js/post.js
@@ -20,6 +20,45 @@ if (newPostForm) {
   newPostForm.addEventListener('submit', newFormHandler)
 }
 
+// edit a single post
+const editFormHandler = async function (event) {
+  event.preventDefault()
+  const postId = document.querySelector('input[name="post-id"]').value
+
+  const title = document.querySelector('input[name="post-title"]').value
+  const blogText = document.querySelector('textarea[name="post-body"]').value
+
+  await fetch(`/api/post/8`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      title,
+      blogText,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+
+  // document.location.replace('/dashboard')
+}
+
+const editPostForm = document.querySelector('#edit-post-form')
+if (editPostForm) {
+  editPostForm.addEventListener('submit', editFormHandler)
+}
+
+const deleteClickHandler = async function () {
+  await fetch(`/api/post/${postId}`, {
+    method: 'DELETE',
+  })
+  document.location.replace('/dashboard')
+}
+
+const deleteBtn = document.querySelector('#delete-btn')
+if (deleteBtn) {
+  deleteBtn.addEventListener('click', deleteClickHandler)
+}
+
 // new comment handler
 const commentFormHandler = async function (event) {
   event.preventDefault()
